357e5f10a1adacc135142cdaae0265ed0f2e921c
Elvis <elvis.e.amancio@gmail.com>
2021-04-26 18:23:52 -0300

    Componente: TransactionsTable

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -3,7 +3,39 @@ import { Container } from './styles';
 export function TransactionsTable() {
   return (
     <Container>
-      <h1>TransactionsTable</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Site</td>
+            <td>R$ 1200,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+
+          <tr>
+            <td>Desenvolvimento de Site</td>
+            <td>R$ 1200,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+
+          <tr>
+            <td>Desenvolvimento de Site</td>
+            <td>R$ 1200,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
     </Container>
   );
 };
